0fa9d3f3ee804c289097999a31b2e2c5e630663d
silviasalete <silviasalete@users.noreply.github.com>
2021-12-02 18:28:27 -0300

    :tada: Initial commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/project-basic-static'),
+      dir: require('path').join(__dirname, './coverage/project-link-static'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'project-basic-static'`, () => {
+  it(`should have as title 'project-link-static'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('project-basic-static');
+    expect(app.title).toEqual('project-link-static');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('project-basic-static app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'project-link-static app is running!'
+    );
   });
 });
